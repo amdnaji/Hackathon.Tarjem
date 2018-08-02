6157105b2b16cf29dff14b1fd746a42fa639b7a4
Ahmed Naji <amd.naji@outlook.com>
2018-08-02 21:39:19 +0300

    added api to translate

---
+++ Hackathon.Tarjem/Hackathon.Tarjem/Controllers/TranslateController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hackathon.Tarjem.Controllers
+{
+    public class TranslateController : Controller
+    {
+        [HttpPost]
+        public IActionResult Start([FromBody]string text)
+        {
+            //Start the trnaslation
+            var body = new
+            {
+                to = "Hi",
+                from = "Welcome"
+            };
+            return Json(body);
+        }
+    }
+}
