c0ab34e4062e3f7e28674e2a0004f1c9be931744
Ahmed Naji <amd.naji@outlook.com>
2018-08-02 18:07:26 +0300

    Uploaded to github

---
+++ Hackathon.Tarjem/Hackathon.Tarjem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hackathon.Tarjem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
